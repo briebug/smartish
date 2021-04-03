229a494931b6286eab24a8c406bb46f71e145d67
jordan powell <jordan.powell@siriusxm.com>
2021-04-03 12:59:23 -0400

    initial commit

---
+++ apps/smartish-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'smartish-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'smartish-app';
+}
