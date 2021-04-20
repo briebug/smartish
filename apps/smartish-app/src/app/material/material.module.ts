db1745c3acb63e42000a8de741520728bbd15a98
jordan powell <jordan.powell@siriusxm.com>
2021-04-20 19:33:01 -0400

    fix(app): add missing material imports

--- apps/smartish-app/src/app/material/material.module.ts
+++ apps/smartish-app/src/app/material/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
@@ -9,6 +9,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -17,22 +18,26 @@ import { MatDividerModule } from '@angular/material/divider';
     MatTableModule,
     MatChipsModule,
     MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
     MatMenuModule,
     MatSelectModule,
     MatListModule,
     MatDividerModule,
+    MatButtonModule,
   ],
   exports: [
     MatCardModule,
     MatTableModule,
     MatChipsModule,
     MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
     MatMenuModule,
     MatSelectModule,
     MatListModule,
     MatDividerModule,
+    MatButtonModule,
   ],
 })
 export class MaterialModule {}
