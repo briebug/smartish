331171b2da6cf61ecce4a7dfaaa24563fa010cf4
jordan powell <jordan.powell@siriusxm.com>
2021-04-20 16:10:48 -0400

    feat(api): create new nestjs api app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/smartish-app', '<rootDir>/libs/ngrx'],
+  projects: [
+    '<rootDir>/apps/smartish-app',
+    '<rootDir>/libs/ngrx',
+    '<rootDir>/apps/api',
+  ],
 };
