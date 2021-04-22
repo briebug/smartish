72a4edc8bf406d34fa184d9c4e69823f08ffe6e3
jordan powell <jordan.powell@siriusxm.com>
2021-04-21 20:43:31 -0400

    fix(ngrx): resolve issue with smartDispatch types

--- libs/ngrx/src/lib/utils/smart-dispatch.ts
+++ libs/ngrx/src/lib/utils/smart-dispatch.ts
@@ -1,20 +1,18 @@
+import { SMARTISH_STORE_TOKEN } from '@briebug/smartish-ngrx';
 import { ActionCreator, Creator, Store } from '@ngrx/store';
-import { FunctionWithParametersType } from '@ngrx/store/src/models';
+import {
+  FunctionWithParametersType,
+  TypedAction,
+} from '@ngrx/store/src/models';
 import { InjectorLocator } from '../injector-locator/injector-locator';
 
-export const smartDispatch = <
-  T extends string = string,
-  C extends Creator = Creator,
-  P extends any[] = any[],
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  R extends object = object
->(
-  action: ActionCreator<T, C>
-): FunctionWithParametersType<P, R> => {
-  const dispatch = (...args: P): R => {
-    const store = (InjectorLocator as any)._injector.get(Store);
-
-    return store.dispatch(action(args));
-  };
-  return dispatch;
+export const smartDispatch = <T extends string, P extends object>(
+  action:
+    | ActionCreator<T, () => TypedAction<T>>
+    | ActionCreator<T, (props: P) => P & TypedAction<T>>
+) => (args?: P) => {
+  const store = (InjectorLocator as any)['_injector'].get(
+    SMARTISH_STORE_TOKEN
+  ) as Store<any>;
+  store.dispatch(action(args) as TypedAction<T>);
 };
